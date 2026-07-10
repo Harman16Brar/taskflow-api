dec0a720865fb1d0467c9cd95704e63f8a7a8c91
Harman <37739820+Harman16Brar@users.noreply.github.com>
2026-07-10 21:50:09 +0530

    Update TaskflowApiApplicationTests.java

--- src/test/java/com/taskflow_api/TaskflowApiApplicationTests.java
+++ src/test/java/com/taskflow_api/TaskflowApiApplicationTests.java
@@ -1,10 +1,11 @@
 package com.taskflow_api;
 
+import com.taskflow_api.shared.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TaskflowApiApplicationTests {
+class TaskflowApiApplicationTests extends BaseIntegrationTest {
 
 	@Test
 	void contextLoads() {
